2c7de96fd41f2a1b57dbe9540426cc0b154b5393
Julius Torralba <Julius.Torralba@GMail.com>
2024-04-19 16:30:58 -0600

    Implement Serilog diagnostic logging library.

--- SERVICE/Program.cs
+++ SERVICE/Program.cs
@@ -1,6 +1,96 @@
 using SERVICE;
 
+using Serilog;
+using Serilog.Events;
+
+int Retention = 365;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+
+    .WriteTo.Console(
+        outputTemplate:
+            "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}")
+
+    .WriteTo.File(@"log\.log", rollingInterval:RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention),
+        outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}")
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Debug)
+        .WriteTo.Map<string>(
+            File => "Debug",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Verbose)
+        .WriteTo.Map<string>(
+            File => "Verbose",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Warning)
+        .WriteTo.Map<string>(
+            File => "Warning",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Information)
+        .WriteTo.Map<string>(
+            File => "Information",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Error)
+        .WriteTo.Map<string>(
+            File => "Error",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Logger(LC => LC
+        .Filter.ByIncludingOnly(Event => Event.Level == LogEventLevel.Fatal)
+        .WriteTo.Map<string>(
+            File => "Fatal",
+                 (Folder, WT) =>
+                    WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromHours(Retention), shared: true,
+                    outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+                 )
+        )
+    )
+
+    .WriteTo.Map("Folder", "System", (Folder, WT) =>
+        WT.File(@$"log\{Folder}\.log", rollingInterval: RollingInterval.Minute, retainedFileTimeLimit: TimeSpan.FromDays(Retention), shared: true,
+        outputTemplate: "[{Level:u3}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Message}{NewLine}"
+        )
+    )
+
+    .CreateLogger();
+
 var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.ClearProviders();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
--- SERVICE/Worker.cs
+++ SERVICE/Worker.cs
@@ -1,23 +1,34 @@
+using Serilog;
+
 namespace SERVICE
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                }
-                await Task.Delay(1000, stoppingToken);
+                //try
+                //{
+                //    int a = 10;
+                //    int b = 0;
+                //    int c = a / b;
+                //}
+                //catch (Exception E)
+                //{
+                //    Log.Error($"{E.Message}");
+                //}
+
+                Log.Verbose("Verbose");
+                Log.Debug("Debug");
+                Log.Warning("Warning");
+                Log.Information("Information");
+                Log.Error("Error");
+                Log.Fatal("Fatal");
+
+                Log.ForContext("Folder", "Data").Information("HelloWorld");
+
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
